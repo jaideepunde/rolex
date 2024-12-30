78026d2501e21a914ef442feb5b35c75eee26caf
jaideep <jaideepunde78@gmail.com>
2024-12-30 12:02:47 +0530

    added suscefully

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String args []){
+        int a,b;
+        a=5;
+        b=10;
+        int sum=a+b;
+        System.out.println("sum");
+    }
+    
+}
